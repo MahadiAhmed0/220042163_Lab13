33a74f95d37b4a78319b3e7e0c36968a8ebd394f
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2025-03-26 19:06:27 +0600

    Constants class for holding constant values

---
+++ src/config/Constants.java
@@ -0,0 +1,8 @@
+public final class Constants {
+    public static final int MAX_ADMINS = 10;
+    public static final int MAX_TICKETS = 10;
+    public static final String ROOT_USERNAME = "root";
+    public static final String ROOT_PASSWORD = "root";
+    
+    private Constants() {}
+}
